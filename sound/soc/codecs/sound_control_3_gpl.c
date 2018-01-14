6e8fcbf8c33d7d8e48e371978f3eb920be80fdfc
gsstudios <josh.lay@exemail.com.au>
2018-01-14 17:44:18 +1100

    Revert "sound_control: remove checksum protection completely"
    This reverts commit ac34e572dd59faa63d5b71319a0905229136df77.

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -182,6 +182,11 @@ int snd_hax_reg_access(unsigned int reg)
 }
 EXPORT_SYMBOL(snd_hax_reg_access);
 
+static bool calc_checksum(unsigned int a, unsigned int b, unsigned int c)
+{
+	return true;
+}
+
 static ssize_t cam_mic_gain_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
@@ -198,9 +203,10 @@ static ssize_t cam_mic_gain_store(struct kobject *kobj,
 
 	sscanf(buf, "%u %u", &lval, &chksum);
 
-	taiko_write(fauxsound_codec_ptr,
-		TAIKO_A_CDC_TX6_VOL_CTL_GAIN, lval);
-
+	if (calc_checksum(lval, 0, chksum)) {
+		taiko_write(fauxsound_codec_ptr,
+			TAIKO_A_CDC_TX6_VOL_CTL_GAIN, lval);
+	}
 	return count;
 }
 
@@ -219,9 +225,10 @@ static ssize_t mic_gain_store(struct kobject *kobj,
 
 	sscanf(buf, "%u %u", &lval, &chksum);
 
-	taiko_write(fauxsound_codec_ptr,
-		TAIKO_A_CDC_TX7_VOL_CTL_GAIN, lval);
-
+	if (calc_checksum(lval, 0, chksum)) {
+		taiko_write(fauxsound_codec_ptr,
+			TAIKO_A_CDC_TX7_VOL_CTL_GAIN, lval);
+	}
 	return count;
 
 }
@@ -230,10 +237,10 @@ static ssize_t speaker_gain_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
         return sprintf(buf, "%u %u\n",
-		taiko_read(fauxsound_codec_ptr,
-			TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL),
-		taiko_read(fauxsound_codec_ptr,
-			TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL));
+			taiko_read(fauxsound_codec_ptr,
+				TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL),
+			taiko_read(fauxsound_codec_ptr,
+				TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL));
 
 }
 
@@ -244,11 +251,12 @@ static ssize_t speaker_gain_store(struct kobject *kobj,
 
 	sscanf(buf, "%u %u %u", &lval, &rval, &chksum);
 
-	taiko_write(fauxsound_codec_ptr,
-		TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL, lval);
-	taiko_write(fauxsound_codec_ptr,
-		TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL, rval);
-
+	if (calc_checksum(lval, rval, chksum)) {
+		taiko_write(fauxsound_codec_ptr,
+			TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL, lval);
+		taiko_write(fauxsound_codec_ptr,
+			TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL, rval);
+	}
 	return count;
 }
 
@@ -256,10 +264,10 @@ static ssize_t headphone_gain_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%u %u\n",
-		taiko_read(fauxsound_codec_ptr,
-			TAIKO_A_CDC_RX1_VOL_CTL_B2_CTL),
-		taiko_read(fauxsound_codec_ptr,
-			TAIKO_A_CDC_RX2_VOL_CTL_B2_CTL));
+			taiko_read(fauxsound_codec_ptr,
+				TAIKO_A_CDC_RX1_VOL_CTL_B2_CTL),
+			taiko_read(fauxsound_codec_ptr,
+				TAIKO_A_CDC_RX2_VOL_CTL_B2_CTL));
 }
 
 static ssize_t headphone_gain_store(struct kobject *kobj,
@@ -269,11 +277,12 @@ static ssize_t headphone_gain_store(struct kobject *kobj,
 
 	sscanf(buf, "%u %u %u", &lval, &rval, &chksum);
 
-	taiko_write(fauxsound_codec_ptr,
-		TAIKO_A_CDC_RX1_VOL_CTL_B2_CTL, lval);
-	taiko_write(fauxsound_codec_ptr,
-		TAIKO_A_CDC_RX2_VOL_CTL_B2_CTL, rval);
-
+	if (calc_checksum(lval, rval, chksum)) {
+		taiko_write(fauxsound_codec_ptr,
+			TAIKO_A_CDC_RX1_VOL_CTL_B2_CTL, lval);
+		taiko_write(fauxsound_codec_ptr,
+			TAIKO_A_CDC_RX2_VOL_CTL_B2_CTL, rval);
+	}
 	return count;
 }
 
@@ -294,6 +303,7 @@ static ssize_t headphone_pa_gain_store(struct kobject *kobj,
 
 	sscanf(buf, "%u %u %u", &lval, &rval, &chksum);
 
+	if (calc_checksum(lval, rval, chksum)) {
 	gain = taiko_read(fauxsound_codec_ptr, TAIKO_A_RX_HPH_L_GAIN);
 	out = (gain & 0xf0) | lval;
 	taiko_write(fauxsound_codec_ptr, TAIKO_A_RX_HPH_L_GAIN, out);
@@ -309,7 +319,7 @@ static ssize_t headphone_pa_gain_store(struct kobject *kobj,
 	status = taiko_read(fauxsound_codec_ptr, TAIKO_A_RX_HPH_R_STATUS);
 	out = (status & 0x0f) | (rval << 4);
 	taiko_write(fauxsound_codec_ptr, TAIKO_A_RX_HPH_R_STATUS, out);
-
+	}
 	return count;
 }
 
@@ -339,10 +349,10 @@ static ssize_t sound_reg_write_store(struct kobject *kobj,
         unsigned int out, chksum;
 
 	sscanf(buf, "%u %u", &out, &chksum);
-
-	if (selected_reg != 0xdeadbeef)
-		taiko_write(fauxsound_codec_ptr, selected_reg, out);
-
+	if (calc_checksum(out, 0, chksum)) {
+		if (selected_reg != 0xdeadbeef)
+			taiko_write(fauxsound_codec_ptr, selected_reg, out);
+	}
 	return count;
 }
 
